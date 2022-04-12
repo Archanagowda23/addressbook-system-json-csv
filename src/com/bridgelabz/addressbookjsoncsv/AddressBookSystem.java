b1395f92dff05cf1b67234432b277986b0da588c
Archanagowda23 <archanar249@gmail.com>
2022-04-12 07:46:59 +0530

    Added welcome message

---
+++ src/com/bridgelabz/addressbookjsoncsv/AddressBookSystem.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.addressbookjsoncsv;
+
+public class AddressBookSystem {
+
+    public static void main(String[] args) {
+        System.out.println("welcome to Address Book Program");
+    }
+}
